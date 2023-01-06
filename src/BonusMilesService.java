4e9b8cd9d2d910ae4e4111d3801a041ec0710b94
Katy test <artebyakinae@mail.ru>
2023-01-06 16:43:57 +0300

    Initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,20 @@
+public class BonusMilesService {
+
+    public int calculate(int cost) {
+        int result;
+        int price = 10000;
+        { result = (price / 20);}
+        return result;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
